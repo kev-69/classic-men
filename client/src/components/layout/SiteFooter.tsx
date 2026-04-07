1e06c8f1127eabd6ee224b8eb2b072854ab7c6ef
kev-69 <bismarkobuobi19@gmail.com>
2026-04-07 12:52:30 +0000

    fix: update social media links in SiteFooter component

--- client/src/components/layout/SiteFooter.tsx
+++ client/src/components/layout/SiteFooter.tsx
@@ -5,11 +5,8 @@ export function SiteFooter() {
       <p>© {new Date().getFullYear()} Classic Men</p>
       
       <div className="social-icons">
-        <a href="https://snapchat.com/t/Es7OCDgr" target="_blank" rel="noopener noreferrer" className="social-icon">
-          SnapChat
-        </a>
-        <a href="https://www.tiktok.com/@the.christ.cultur?_t=ZM-8ug3MPoZh1q&_r=1" target="_blank" rel="noopener noreferrer" className="social-icon">
-          Tiktok
+        <a href="https://www.instagram.com/casual_men_fit?igsh=MThkdDJxdTJ3Z2oxMQ==/" target="_blank" rel="noopener noreferrer" className="social-icon">
+          Instagram
         </a>
       </div>
     </footer>
